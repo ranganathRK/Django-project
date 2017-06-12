c38d9ec0d7d6d60176ee33ce529e6ac5f38e2e3c
ranganathRK <ranganathk.pethri@gmail.com>
2017-06-12 10:07:33 +0530

    finalchange

--- foodblog/views.py
+++ foodblog/views.py
@@ -31,7 +31,7 @@ def post_detail(request,year,month,day,post):
 		comment_form=CommentForm(data=request.POST)
 		if comment_form.is_valid():
 			new_comment=comment_form.save(commit=False)
-			new_comment.post=Post
+			new_comment.post=post
 			new_comment.save()
 	else:
 		comment_form=CommentForm()
@@ -39,7 +39,7 @@ def post_detail(request,year,month,day,post):
 		{'post':post,
 		'comments':comments,
 		'comment_form':comment_form})	 
-
+	
 def post_share(request, post_id):
  # Retrieve post by id 
 	post = get_object_or_404(Post, id=post_id, status='published') 
@@ -51,7 +51,7 @@ def post_share(request, post_id):
 			post_url = request.build_absolute_uri(post.get_absolute_url())
 			subject = '{} ({}) recommends you reading "{}"'. format(cd['name'], cd['email'], post.title) 
 			message = 'Read "{}" at {}\n\n{}\'s comments: {}'. format(post.title, post_url, cd['name'], cd['comments']) 
-			send_mail(subject, message, 'admin@foodblog.com', [cd['to']]) 
+			send_mail(subject, message, 'ranganathk.pethri@gmail.com', [cd['to']]) 
 			sent = True
 	else: 
 		form = EmailPostForm() 
